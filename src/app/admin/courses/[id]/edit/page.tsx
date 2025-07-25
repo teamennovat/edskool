82ecf6e42e41429e55108f156bec6b480ce1ca4b
mashcatg <120169459+mashcatg@users.noreply.github.com>
2025-07-25 21:51:29 +0600

    zaz

--- src/app/admin/courses/[id]/edit/page.tsx
+++ src/app/admin/courses/[id]/edit/page.tsx
@@ -1,17 +1,13 @@
 import CourseForm from "../../_components/course-form";
-import { Suspense } from "react";
 
-interface PageProps {
+export default function Page({
+  params,
+}: {
   params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default function EditCoursePage({ params, searchParams }: PageProps) {
+}) {
   return (
-    <Suspense fallback="Loading...">
-      <div className="container max-w-4xl py-6">
-        <CourseForm courseId={params.id} />
-      </div>
-    </Suspense>
+    <div className="container max-w-4xl py-6">
+      <CourseForm courseId={params.id} />
+    </div>
   );
 }
