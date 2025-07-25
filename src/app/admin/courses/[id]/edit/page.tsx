ee4bd029af98400fea0f5b6bd855bc0ab087122c
mashcatg <120169459+mashcatg@users.noreply.github.com>
2025-07-25 21:09:03 +0600

    async removed and updated editcoursepage

--- src/app/admin/courses/[id]/edit/page.tsx
+++ src/app/admin/courses/[id]/edit/page.tsx
@@ -1,10 +1,12 @@
 import CourseForm from "../../_components/course-form";
-interface EditCoursePageProps {
+
+type Props = {
   params: {
     id: string;
   };
-}
-export default function EditCoursePage({ params }: EditCoursePageProps) {
+};
+
+export default function EditCoursePage({ params }: Props) {
   return (
     <div className="container max-w-4xl py-6">
       <CourseForm courseId={params.id} />
